1a12633dfa862355165dc4991d6b8f769548c082
Fadi-Nayef <adraf38@hotmail.com>
2021-03-22 23:16:37 +0200

    starting with css

--- javafiles/PPE.js
+++ javafiles/PPE.js
@@ -1,37 +1,36 @@
 
 
-var emploies=prompt('how many employer do you have');
+var emploies = prompt('how many employer do you have');
 
 
-if (emploies >=10 && emploies<=20){
-    alert('you welcome');}
-        else if (emploies <10 || emploies>20)
+if (emploies >= 10 && emploies <= 20) {
+    alert('you welcome');
+}
+else if (emploies < 10 || emploies > 20) {
+    alert('sorry your capacity dosent match')
+}
+for (var i = 0; i < 5; i++) {
 
-            {
-            alert('sorry your capacity dosent match')
-        }
-    for(var i=0 ; i<5 ; i++ ){
+}
 
-    }
 
-    
 var mask = prompt("which mask is better full face or half face?");
- while (mask !=='half' && mask!=='full'){
-     mask=prompt('please write full or half only');
+while (mask !== 'half' && mask !== 'full') {
+    mask = prompt('please write full or half only');
 
- }
+}
 
 
-var numberOfShows=prompt('how many resp you need?');
+var numberOfShows = prompt('how many resp you need?');
 
-var choosen='';
+var choosen = '';
 
-for(var i=0 ; i<numberOfShows ; i++){
+for (var i = 0; i < numberOfShows; i++) {
 
     if (mask == 'full') {
-        choosen +='<img src="javafiles/6800_4.jpg"/></br>';
-    } else if (mask =='half') {
-        choosen +='<img src="javafiles/6200.jpg"/></br>';
+        choosen += '<img src="javafiles/6800_4.jpg"/></br>';
+    } else if (mask == 'half') {
+        choosen += '<img src="javafiles/6200.jpg"/></br>';
     } else {
         alert('you need to type full or half ');
     }
